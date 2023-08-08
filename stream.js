e01996e16dbd59db9e2a4b00098b60aa3b41de45
Criss Ayo <anayocrescent2@gmail.com>
2023-08-08 22:59:59 +0100

    add a stream reader

---
+++ stream.js
@@ -0,0 +1,32 @@
+const fs = require("fs");
+
+// Function to process data in smaller chunks
+function processDataInChunks(chunk) {
+  // In this example, we'll just convert the chunk to uppercase
+  return chunk.toString().toUpperCase();
+}
+
+// Using streams to process data in smaller chunks and avoid excessive Buffers
+function processFileWithStreams(inputFilePath, outputFilePath) {
+  const readStream = fs.createReadStream(inputFilePath);
+  const writeStream = fs.createWriteStream(outputFilePath);
+
+  readStream.on("data", (chunk) => {
+    const processedChunk = processDataInChunks(chunk);
+    writeStream.write(processedChunk);
+  });
+
+  readStream.on("end", () => {
+    writeStream.end();
+    console.log("Data processing completed.");
+  });
+
+  readStream.on("error", (err) => {
+    console.error("Error reading the file:", err);
+  });
+}
+
+const inputFilePath = "large_input_file.txt";
+const outputFilePath = "output.txt";
+
+processFileWithStreams(inputFilePath, outputFilePath);
